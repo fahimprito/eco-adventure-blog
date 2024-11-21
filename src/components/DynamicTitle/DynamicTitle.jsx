54b16a64c1bc6270c45bee9bf90ad5401a6ef8a5
fahimprito <ahmmedahmmed75@gmail.com>
2024-11-21 20:23:13 +0600

    dynamic title added

---
+++ src/components/DynamicTitle/DynamicTitle.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const DynamicTitle = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        let title;
+
+        if (location.pathname.startsWith("/adventure/")) {
+            title = "Adventure Details | EcoTravelers";
+        } else {
+            const pageTitles = {
+                "/": "Home | EcoTravelers",
+                "/about": "About Us | EcoTravelers",
+                "/updateprofile": "Update Profile | EcoTravelers",
+                "/myprofile": "My Profile | EcoTravelers",
+                "/adventures": "Adventures | EcoTravelers",
+                "/adventure/:id": "Adventure Details | EcoTravelers",
+                "/login": "Login | EcoTravelers",
+                "/register": "Register | EcoTravelers",
+            };
+            
+            title = pageTitles[location.pathname] || "Explore Nature | EcoTravelers";
+        }
+
+        document.title = title;
+
+    }, [location]);
+
+    return null;
+};
+
+export default DynamicTitle;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import DynamicTitle from "../components/DynamicTitle/DynamicTitle";
 
 
 const MainLayout = () => {
     return (
         <div className="font-montserrat">
             <Navbar></Navbar>
+            <DynamicTitle></DynamicTitle>
             <div  className="min-h-[50vh]">
                 <Outlet></Outlet>
             </div>
